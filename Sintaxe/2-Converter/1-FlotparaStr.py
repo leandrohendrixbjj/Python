dcafc588547d4e417ddb6f25eeb2996ae06ef0d1
leandro starks <leandrotully@gmail.com>
2025-03-31 20:01:36 -0300

    Tipos de dados

---
+++ Sintaxe/2-Converter/1-FlotparaStr.py
@@ -0,0 +1,9 @@
+# Tipo float
+altura = 1.75
+
+data = str(altura)
+
+
+print('Tipo:', type(data)) # Saída: <class 'str'>
+
+print('Tipo', type(altura)) # Saída: <class 'float'>
---
+++ index.py
@@ -0,0 +1,5 @@
+# Conversão de int para float
+idade = 20
+idade_float = float(idade)
+
+print('Tipo:', type(idade_float)) # Saída: <class 'float'>
