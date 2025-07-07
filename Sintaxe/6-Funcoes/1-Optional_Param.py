ddd3ac0dfe0ae5eda67eb6a090f9bfdecfb974ca
leandro starks <leandrotully@gmail.com>
2025-07-07 06:19:58 -0300

    Sintaxe

--- Sintaxe/6-Funcoes/1-Optional_Param.py
+++ Sintaxe/6-Funcoes/1-Optional_Param.py
@@ -1,15 +1,14 @@
+
 from typing import Optional
 from helper.clear import *
 
 clear()
 
-def getName(name: Optional[str] = None) -> str:
-    if name is None:
-        return 'Soares'
-    return name
+def getName(name: Optional[str] = None, default_name: str = "Soares") -> str:
+    if name is not None:
+        return name
+    return default_name
 
 data = getName('leandro')
 print(data)  # Resultado: leandro
 
-data2 = getName()
-print(data2)  # Resultado: Soares
